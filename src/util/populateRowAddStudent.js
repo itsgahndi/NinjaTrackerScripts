d85c2fa01002d67c94fb86ef53b10cd3e05d77fd
itsgahndi <58355958+itsgahndi@users.noreply.github.com>
2020-03-16 17:54:58 -0400

    Update populateRowAddStudent.js

--- src/util/populateRowAddStudent.js
+++ src/util/populateRowAddStudent.js
@@ -1,3 +1,10 @@
-function populateRowAddStudent() {
-  Logger.log('I work!!');
+function populateRowAddStudent(firstName, lastName, dateEnrolled, dojoUsername, scratchUsername, scratchPassword) {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheets = spreadsheet.getSheets();
+
+  for (var i = 0; i < sheets.length(); i++) {
+    if ((i != 0 ) || (i != 3) || (i != 5) || (i != 7)) {
+      
+    }
+  }
 }
