83a14a9a3d0f6d12c968d40882d938913e4720e9
itsgahndi <58355958+itsgahndi@users.noreply.github.com>
2020-03-16 16:34:34 -0400

    Moved functions to new file

--- src/studentManagement.js
+++ src/studentManagement.js
@@ -23,33 +23,3 @@ function remStudent() {
     .setWidth(500);
   ui.showModalDialog(html, 'Remove Student');
 }
-
-// Iterates through "Roster" sheet in column B6:B to find largest
-// number and return the next number.
-function getNewID() {
-  var rosterID = SpreadsheetApp.getActiveSpreadsheet()
-    .getSheetByName('Roster')
-    .getRange("B6:B");
-  var values = rosterID.getValues();
-
-  var tempLarge = 0;
-  var next;
-
-  for (var i = 1; i < rosterID.getLastRow() - 5; i++) {
-    next = values[i][0];
-    if (next > tempLarge)
-      tempLarge = next;
-  }
-  return tempLarge + 1;
-}
-
-function processData(newStudent) {
-  var firstName = newStudent.firstName;
-  var lastName = newStudent.lastName;
-  var dateEnrolled = newStudent.dateEnrolled;
-  var dojoUsername = newStudent.dojoUsername;
-  var scratchUsername = newStudent.scratchUsername;
-  var scratchPassword = newStudent.scratchPassword;
-
-  Logger.log(firstName + " " + lastName + " " + dateEnrolled);
-}
