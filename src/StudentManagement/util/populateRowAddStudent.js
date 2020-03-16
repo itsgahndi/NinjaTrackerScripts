6f2a1c0a29f807218000a652e4fa5fbe049c01ce
itsgahndi <58355958+itsgahndi@users.noreply.github.com>
2020-03-16 18:12:13 -0400

    Update populateRowAddStudent.js

--- src/StudentManagement/util/populateRowAddStudent.js
+++ src/StudentManagement/util/populateRowAddStudent.js
@@ -2,9 +2,9 @@ function populateRowAddStudent(firstName, lastName, dateEnrolled, dojoUsername, 
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   var sheets = spreadsheet.getSheets();
 
-  for (var i = 0; i < sheets.length(); i++) {
+  for (var i = 0; i < sheets.length; i++) {
     if ((i != 0 ) || (i != 3) || (i != 5) || (i != 7)) {
-      
+
     }
   }
 }
