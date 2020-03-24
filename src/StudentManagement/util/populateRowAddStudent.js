a6bd49666a231d90d064180b264a4283c5a0c8e8
itsgahndi <58355958+itsgahndi@users.noreply.github.com>
2020-03-24 03:59:15 -0400

    Moved individual functions as seperate file

--- src/StudentManagement/util/populateRowAddStudent.js
+++ src/StudentManagement/util/populateRowAddStudent.js
@@ -1,10 +1,3 @@
 function populateRowAddStudent(firstName, lastName, dateEnrolled, dojoUsername, scratchUsername, scratchPassword) {
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  var sheets = spreadsheet.getSheets();
-
-  for (var i = 0; i < sheets.length; i++) {
-    if ((i != 0 ) || (i != 3) || (i != 5) || (i != 7)) {
-
-    }
-  }
+  roster(firstName, lastName, dateEnrolled, dojoUsername, scratchUsername, scratchPassword);
 }
