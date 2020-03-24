c496730dca2e7ce9658408dc3849674d31d671f3
itsgahndi <58355958+itsgahndi@users.noreply.github.com>
2020-03-24 04:08:01 -0400

    Update roster.js

--- src/StudentManagement/util/populateRowAddStudent/roster.js
+++ src/StudentManagement/util/populateRowAddStudent/roster.js
@@ -5,13 +5,12 @@ function roster(firstName, lastName, dateEnrolled, dojoUsername, scratchUsername
 
   rosterSheet.insertRowAfter(lastRow);
 
-  var range = rosterSheet.getRange(lastRow, 2, 1, lastColumn - 1);
+  var activeRange = rosterSheet.getRange(lastRow, 2, 1, lastColumn - 1);
+  var formulasRange = rosterSheet.getRange(lastRow - 1, 2, 1, lastColumn - 1);
   var beltFormulaCell = rosterSheet.getRange(lastRow, 5);
-
-  var beltFormula = "=IFS(VLOOKUP(B72,'Black Belt'!B70:G,6,FALSE)=TRUE, "Complete Program",VLOOKUP(B72,'Red Belt'!B70:G,6,FALSE)=TRUE, "Black Belt",VLOOKUP(B72,'Brown Belt'!B70:G,6,FALSE)=TRUE, "Red Belt",VLOOKUP(B72,'Purple Belt'!B70:G,6,FALSE)=TRUE, "Brown Belt",VLOOKUP(B72,'Blue Belt'!B70:G,6,FALSE)=TRUE, "Purple Belt",VLOOKUP(B72,'Green Belt'!B70:G,6,FALSE)=TRUE, "Blue Belt",VLOOKUP(B72,'Orange Belt'!B70:G,6,FALSE)=TRUE, "Green Belt",VLOOKUP(B72,'Yellow Belt V3'!B70:G,6,FALSE)=TRUE, "Orange Belt",VLOOKUP(B72,'Yellow Belt V2'!B70:G,6,FALSE)=TRUE, "Orange Belt",VLOOKUP(B72,'White Belt V3'!B70:G,6,FALSE)=TRUE, "Yellow Belt",VLOOKUP(B72,FUNdamentals!B70:G,6,FALSE)=TRUE, "White Belt", 1=1, "FUNdamentals")";
   var status = "Active";
   var values = [getNewID(), firstName, lastName, "", dateEnrolled, status, dojoUsername, scratchUsername, scratchPassword];
 
-  range.setValues(values);
-  beltFormulaCell.setFormula(beltFormula);
+  activeRange.setValues(values);
+  activeRange.setFormulas(formulasRange);
 }
